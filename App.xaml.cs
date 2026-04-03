24d729cf4af6750d4618d7a502e1739df343fa5e
antonlosk <antonloskutovqwerty@gmail.com>
2026-04-03 17:35:58 +0500

    Update App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,8 +1,23 @@
+using System;
 using System.Windows;
 
 namespace DNStoHOSTS
 {
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // Перехват всех необработанных ошибок
+            AppDomain.CurrentDomain.UnhandledException += (s, ex) => 
+                MessageBox.Show(ex.ExceptionObject.ToString(), "Критическая ошибка");
+
+            DispatcherUnhandledException += (s, ex) => 
+            {
+                MessageBox.Show(ex.Exception.ToString(), "Ошибка интерфейса");
+                ex.Handled = true;
+            };
+
+            base.OnStartup(e);
+        }
     }
 }
